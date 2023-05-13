7961a2e547763c6c97a54d1b1f1c527aa87669ca
Owen Dechow <owen,dechow@gmail.com>
2023-05-13 19:05:38 -0400

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-#inbreeding-python
+# inbreeding-python
+Inbreeding Coefficient Calculator For Pedigrees
+
+## How to use
+___
+### 1. Create a python dict containing the pedigree.
+```python
+# Pedigree example with 0.25 inbreeding coefficient (25%)
+pedigree = {
+    "sire": {
+        "sire": None,
+        "dam": None,
+        "id": "2834",
+    },
+    "dam": {
+        "sire": {
+            "sire": None,
+            "dam": None,
+            "id": "2834",
+        },
+        "dam": None,
+        "id": "781",
+    },
+    "id": "622",
+}
+```
+All animals in pedigree must have a unique id. If the sire/dam of an animal is unknown the value can be set to None or the key can be omitted all together.
+
+___
+
+### 2. Create an instance of the InbreedingCalculator class and load pedigree.
+```python
+calculator = InbreedingCalculator(pedigree)
+```
+or
+```python
+calculator = InbreedingCalculator()
+calculator.pedigree = pedigree
+```
+
+___
+
+### 3. If you used keys other than sire, dam & id ensure you configure your InbreedingCalculator for them.
+```python
+pedigree = {
+"Father": None,
+"Mother": None,
+"Name": "Billy"
+}
+
+calculator = InbreedingCalculator(pedigree, "Father", "Mother", "Name")
+```
+or
+```python
+calculator = InbreedingCalculator(pedigree)
+calculator.sire_key = "Father"
+calculator.dam_key = "Mother"
+calculator.id_key = "Name"
+```
+
+___
+### 4. Use the get_coefficient method to find the inbreeding percentage
+```python
+pedigree = {
+    "sire": {
+        "sire": None,
+        "dam": None,
+        "id": "2834",
+    },
+    "dam": {
+        "sire": {
+            "sire": None,
+            "dam": None,
+            "id": "2834",
+        },
+        "dam": None,
+        "id": "781",
+    },
+    "id": "622",
+}
+
+calculator = InbreedingCalculator(pedigree)
+ic = calculator.get_coefficient()
+print(f"Inbreeding Coefficient: {ic} ({ic * 100}%)")
+```
+Output
+```
+Inbreeding Coefficient: 0.25 (25.0%)
+````
--- inbreeding.py
+++ inbreeding.py
@@ -1,43 +1,45 @@
 class InbreedingCalculator:
     def __init__(
         self,
-        pedagree: dict = {},
+        pedigree: dict = {},
         sire_key: str = "sire",
         dam_key: str = "dam",
         id_key: str = "id",
     ):
-        self.pedagree = pedagree
+        self.pedigree = pedigree
         self.sire_key = sire_key
         self.dam_key = dam_key
         self.id_key = id_key
 
-    def get_coefficient(self):
+    def get_coefficient(self) -> float:
         """Get the inbreeding coefficient of the loaded pedigree"""
 
-        p = self.pedagree
+        p = self.pedigree
 
-        paternal = self._map_parents(p[self.sire_key], {}, 0, "Y")
-        fraternal = self._map_parents(p[self.dam_key], {}, 0, "X")
+        if self.sire_key in p and p[self.sire_key]:
+            paternal = self._map_parents(p[self.sire_key], {}, 0, "Y")
+        if self.dam_key in p and p[self.dam_key]:
+            maternal = self._map_parents(p[self.dam_key], {}, 0, "X")
 
         inbreeding = 0
         for animal_paternal, depths_paternal in paternal.items():
-            if animal_paternal in fraternal:
-                for depth_fraternal in fraternal[animal_paternal]:
+            if animal_paternal in maternal:
+                for depth_maternal in maternal[animal_paternal]:
                     for depth_paternal in depths_paternal:
-                        inbreeding += 0.5 ** (depth_paternal + depth_fraternal + 1)
+                        inbreeding += 0.5 ** (depth_paternal + depth_maternal + 1)
 
         return inbreeding
 
-    def _map_parents(self, p, dic, depth, sex):
+    def _map_parents(self, p, dic, depth, sex) -> dict:
         if p[self.id_key] + sex in dic:
             dic[p[self.id_key] + sex].append(depth)
         else:
             dic[p[self.id_key] + sex] = [depth]
 
-        if p[self.sire_key]:
+        if self.sire_key in p and p[self.sire_key]:
             dic = self._map_parents(p[self.sire_key], dic, depth + 1, "Y")
 
-        if p[self.dam_key]:
+        if self.sire_key in p and p[self.dam_key]:
             dic = self._map_parents(p[self.dam_key], dic, depth + 1, "X")
 
         return dic
